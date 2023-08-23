19503f610f1cb97d09b9f6b6b734d85142edbc23
unknown <pulatabdulhafizov2003@gmail.com>
2023-08-23 12:33:26 +0500

    my first react project

---
+++ src/components/app/app.js
@@ -0,0 +1,24 @@
+import "./app.css"
+import AppInfo from "../app-Info/app-Info"
+import Searchpanel from "../search-panel/search-panel"
+import AppFilter from "../app-filter/app-filter"
+import MovieList from "../movie-list/movie-list"
+import MoviesAddForm from "../movies-add-form/movies-add-form"
+
+const App = () => {
+  return (
+    <div className="app font-monospace">
+        <div className="content">
+            <AppInfo/>
+            <div className="search-panel">
+                <Searchpanel/>
+                <AppFilter/>
+            </div>
+            <MovieList/>
+            <MoviesAddForm/>
+        </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,6 @@
+import "./search-panel.css"
+const Searchpanel = () => {
+  return <input type="text" className="form-control search-input" placeholder="Kinolarni qidirish" />
+}
+
+export default Searchpanel
